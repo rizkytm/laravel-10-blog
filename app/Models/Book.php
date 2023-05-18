52186fe7e95818be5a3949b80259a4b937bbd06e
rizkytm <ritrime97@gmail.com>
2023-05-18 19:42:24 +0700

    first commit

---
+++ app/Models/Book.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+
+class Book extends Eloquent
+
+{
+
+	protected $connection = 'mongodb';
+
+	protected $collection = 'books';
+
+
+    /**
+
+     * The attributes that are mass assignable.
+
+     *
+
+     * @var array
+
+     */
+
+    protected $fillable = [
+
+        'name', 'detail'
+
+    ];
+
+}
